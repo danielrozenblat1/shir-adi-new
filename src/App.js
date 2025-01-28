9b16dbc846373e809244a2e84ef55ca2875f45bc
Your Name <you@example.com>
2025-01-28 11:08:07 +0200

    addedSecondScreen

--- src/App.js
+++ src/App.js
@@ -3,9 +3,11 @@ import './App.css';
 import ByMe from './components/ByMe/ByMe';
 import ForthScreen from './screens/ForthScreen';
 import ThirdScreen from './screens/ThirdScreen';
+import SecondScreen from './screens/SecondScreen';
 
 function App() {
   return <>
+  <SecondScreen/>
   <ThirdScreen/>
   <ForthScreen/>
   <ByMe/>
