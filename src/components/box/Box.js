995003a0f5eda80ef2bf5d5d539e3943af60c639
Your Name <you@example.com>
2025-01-28 11:13:43 +0200

    deletedScrollreveal

--- src/components/box/Box.js
+++ src/components/box/Box.js
@@ -1,7 +1,7 @@
 import { useEffect, useRef } from "react";
 import styles from "./Box.module.css"
 import { Player } from "@lordicon/react";
-import ScrollReveal from "scrollreveal";
+// import ScrollReveal from "scrollreveal";
 const Box=(props)=>{
     const handleComplete = () => {
         setTimeout(() => {
@@ -13,40 +13,40 @@ const Box=(props)=>{
       playerRef1?.current?.playFromBeginning();
     },[])
     useEffect(()=>{
-      ScrollReveal().reveal(`.${styles.title}`, {
-          duration: 1000,
-          distance: "30px",
-          origin: "right", // Start from the right side
-          easing: "ease-out",
-          reset:false,
-          viewFactor: 0.2,
-          interval: 300, // Delay between each element
-          delay: 200, // Delay before the animation starts
-          scale: 1, // Set scale to 1 or null
-        });
-        ScrollReveal().reveal(`.${styles.description}`, {
-          duration: 1000,
-          distance: "30px",
-          origin: "bottom", // Start from the right side
-          easing: "ease-out",
-          reset:false,
-          viewFactor: 0.2,
-          interval: 300, // Delay between each element
-          delay: 200, // Delay before the animation starts
-          scale: 1, // Set scale to 1 or null
-        });
+      // ScrollReveal().reveal(`.${styles.title}`, {
+      //     duration: 1000,
+      //     distance: "30px",
+      //     origin: "right", // Start from the right side
+      //     easing: "ease-out",
+      //     reset:false,
+      //     viewFactor: 0.2,
+      //     interval: 300, // Delay between each element
+      //     delay: 200, // Delay before the animation starts
+      //     scale: 1, // Set scale to 1 or null
+      //   });
+      //   ScrollReveal().reveal(`.${styles.description}`, {
+      //     duration: 1000,
+      //     distance: "30px",
+      //     origin: "bottom", // Start from the right side
+      //     easing: "ease-out",
+      //     reset:false,
+      //     viewFactor: 0.2,
+      //     interval: 300, // Delay between each element
+      //     delay: 200, // Delay before the animation starts
+      //     scale: 1, // Set scale to 1 or null
+      //   });
       
-        ScrollReveal().reveal(`.${styles.icon}`, {
-          duration: 1000,
-          distance: "30px",
-          origin: "left", // Start from the right side
-          easing: "ease-out",
-          reset:false,
-          viewFactor: 0.2,
-          interval: 300, // Delay between each element
-          delay: 200, // Delay before the animation starts
-          scale: 1, // Set scale to 1 or null
-        });
+      //   ScrollReveal().reveal(`.${styles.icon}`, {
+      //     duration: 1000,
+      //     distance: "30px",
+      //     origin: "left", // Start from the right side
+      //     easing: "ease-out",
+      //     reset:false,
+      //     viewFactor: 0.2,
+      //     interval: 300, // Delay between each element
+      //     delay: 200, // Delay before the animation starts
+      //     scale: 1, // Set scale to 1 or null
+      //   });
     },[])
 return <>
 <div className={styles.box}>
--- src/components/can/Can.js
+++ src/components/can/Can.js
@@ -1,6 +1,6 @@
 import React, { useEffect, useRef } from 'react';
 import { Player } from '@lordicon/react';
-import ScrollReveal from "scrollreveal"
+// import ScrollReveal from "scrollreveal"
 import styles from './Can.module.css';
 
 const Can = ({ text,icon }) => {
@@ -15,17 +15,17 @@ const Can = ({ text,icon }) => {
     },[])
     useEffect(()=>{
  
-        ScrollReveal().reveal(`.${styles.text}`, {
-          duration: 1000,
-          distance: "100px",
-          origin: "left", // Start from the right side
-          easing: "ease-out",
-          reset:false,
-          viewFactor: 0.2,
-          interval: 300, // Delay between each element
-          delay: 200, // Delay before the animation starts
-          scale: 1, // Set scale to 1 or null
-        });
+        // ScrollReveal().reveal(`.${styles.text}`, {
+        //   duration: 1000,
+        //   distance: "100px",
+        //   origin: "left", // Start from the right side
+        //   easing: "ease-out",
+        //   reset:false,
+        //   viewFactor: 0.2,
+        //   interval: 300, // Delay between each element
+        //   delay: 200, // Delay before the animation starts
+        //   scale: 1, // Set scale to 1 or null
+        // });
     },[])
   return (
     <div className={styles.container}>
