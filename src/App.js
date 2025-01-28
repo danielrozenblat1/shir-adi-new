e1b899fcb974ac228fa431c99dd9b6fc84871845
Your Name <you@example.com>
2025-01-28 11:01:32 +0200

    addedQuestions

--- src/App.js
+++ src/App.js
@@ -1,9 +1,11 @@
 import logo from './logo.svg';
 import './App.css';
 import ByMe from './components/ByMe/ByMe';
+import ForthScreen from './screens/ForthScreen';
 
 function App() {
   return <>
+  <ForthScreen/>
   <ByMe/>
   
   </>
