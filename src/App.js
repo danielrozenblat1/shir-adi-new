a82c99a34297598dc674adcd44a86d77f4eb05ef
Your Name <you@example.com>
2025-01-28 11:04:14 +0200

    newProuct

--- src/App.js
+++ src/App.js
@@ -2,9 +2,11 @@ import logo from './logo.svg';
 import './App.css';
 import ByMe from './components/ByMe/ByMe';
 import ForthScreen from './screens/ForthScreen';
+import ThirdScreen from './screens/ThirdScreen';
 
 function App() {
   return <>
+  <ThirdScreen/>
   <ForthScreen/>
   <ByMe/>
   
